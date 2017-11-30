b080273d073d6f9a21c76b64c0c17b1781e211ff
AK <akihidis@gmail.com>
2017-11-30 12:01:04 +0000

    * Enhanced Repository to create key space & table

--- src/main/java/Repository.java
+++ src/main/java/Repository.java
@@ -1,4 +1,7 @@
 import com.datastax.driver.core.*;
+import com.datastax.driver.core.schemabuilder.SchemaBuilder;
+import com.datastax.driver.core.schemabuilder.SchemaStatement;
+import com.google.common.collect.ImmutableMap;
 
 import java.time.Instant;
 import java.util.List;
@@ -17,11 +20,36 @@ public class Repository {
             session.prepare("SELECT * FROM TRANSACTIONS");
 
     private static Session createSession() {
-        Cluster cluster = Cluster.builder().addContactPoint("localhost").build();
+        Session session = createKeyspace();
 
-        Session session = cluster.connect("transactions_poc");
+        createTransactionsTable(session);
 
         return session;
+    }
+
+    private static Session createKeyspace() {
+        Cluster cluster = Cluster.builder().addContactPoint("localhost").build();
+
+        SchemaStatement statement = SchemaBuilder.createKeyspace("transactions_poc").ifNotExists()
+                                        .with()
+                                        .replication(ImmutableMap.of("class", "SimpleStrategy","replication_factor",1));
+
+        Session session = cluster.connect();
+
+        session.execute(statement);
+        session.close();
+
+        session = cluster.connect("transactions_poc");
+        return session;
+    }
+
+    private static void createTransactionsTable(Session session) {
+        SchemaStatement transactionsTableStatement = SchemaBuilder.createTable("transactions").ifNotExists()
+                                                .addPartitionKey("date", DataType.text())
+                                                .addClusteringColumn("transaction_id", DataType.text())
+                                                .addColumn("modified", DataType.time());
+
+        session.execute(transactionsTableStatement);
     }
 
     public static Statement getInsertTransactionStatement(Transaction transaction) {
