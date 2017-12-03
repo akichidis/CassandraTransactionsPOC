49bba84a2286820f8b25ad64d889a53ac6649abc
Tasos Kichidis <akihidis@gmail.com>
2017-12-03 13:27:38 +0000

    Enhanced unit tests. Added one more test that presents that is impossible to have a batch statement, with multiple lightweight transaction queries (aka they use IF NOT EXISTS statement) that span on multiple partitions

--- src/test/java/TransactionTests.java
+++ src/test/java/TransactionTests.java
@@ -1,5 +1,6 @@
 import com.datastax.driver.core.BatchStatement;
 import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.exceptions.InvalidQueryException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -48,6 +49,20 @@ public class TransactionTests {
         Assert.assertEquals(0, recordsWithOtherThanInitialTime);
     }
 
+    @Test(expected = InvalidQueryException.class)
+    public void batchIsFailedWithSpannedQueriesOnMultiplePartitions() {
+        BatchStatement batch;
+
+        Instant nowFirstBatch = Instant.now();
+
+        //create the transaction and add them to the batch
+        batch = createSpannedTransactionsBatchStatement(nowFirstBatch);
+
+        //execute the batch - it should throw an InvalidQueryException because it spans on multiple partitions with
+        //having IF NOT EXISTS statements
+        Repository.session.execute(batch);
+    }
+
     private BatchStatement createTransactionsBatchStatement(Instant now) {
         BatchStatement batch = new BatchStatement();
 
@@ -57,6 +72,23 @@ public class TransactionTests {
         transactions.add(new Transaction("29-11-2017","1", now));
         transactions.add(new Transaction("29-11-2017","2", now));
         transactions.add(new Transaction("29-11-2017","3", now));
+
+        Collections.shuffle(transactions);
+
+        transactions.forEach(t -> batch.add(Repository.getInsertTransactionStatement(t)));
+
+        return batch;
+    }
+
+    private BatchStatement createSpannedTransactionsBatchStatement(Instant now) {
+        BatchStatement batch = new BatchStatement();
+
+        List<Transaction> transactions = new ArrayList<>();
+
+        //create the transaction and add them to the batch
+        transactions.add(new Transaction("28-11-2017","1", now));
+        transactions.add(new Transaction("29-11-2017","2", now));
+        transactions.add(new Transaction("30-11-2017","3", now));
 
         Collections.shuffle(transactions);
 
